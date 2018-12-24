5908f4c554c5afaff01a89c2cae82baf4c8c74fc
landwatersun <test@test.com>
2018-12-24 11:04:19 +0300

    Replaced densenet with inception

--- Src/KerasModels.py
+++ Src/KerasModels.py
@@ -1,18 +1,18 @@
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
 
-from keras.applications.densenet import DenseNet121
-from keras.applications.densenet import preprocess_input, decode_predictions
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.applications.inception_resnet_v2 import preprocess_input, decode_predictions
 import PIL.Image
 
 import numpy as np
-from keras_preprocessing import image
+from keras.preprocessing import image
 
 #Load pre-trained model from Internetto
-model121 = DenseNet121(weights='imagenet')
+model = InceptionResNetV2(weights='imagenet')
 
 #Load an image for recognition
-img_path = '../Data/PretrainedModel/1.png'
+img_path = '../Data/PretrainedModel/4.png'
 #Should be of the size described in a documentation of the model, i.e. 224x224 for DenseNet
 img = image.load_img(img_path, target_size=(224, 224))
 #Convert image into numpy array of pixels
@@ -21,7 +21,7 @@ x = np.expand_dims(x, axis=0)
 #Prepare input for recognition
 x = preprocess_input(x)
 
-preds121 = model121.predict(x)
+preds121 = model.predict(x)
 # decode the results into a list of tuples (class, description, probability)
 #  (one such list for each sample in the batch)
 decoded121 = decode_predictions(preds121, top=5)
@@ -30,4 +30,4 @@ for i in range(0, len(decoded121[0])):
     #Change variable in order to print other model's result
     labelTemp = decoded121[0][i]
     # print the classification
-print('%s (%.2f%%)\n' % (labelTemp[1], labelTemp[2]*100))
+print('%s (%.2f%%)\n' % (labelTemp[1], labelTemp[2]))
